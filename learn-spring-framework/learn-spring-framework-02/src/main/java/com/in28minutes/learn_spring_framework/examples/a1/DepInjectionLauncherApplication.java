58f8611314e5fb0d47821afc6b2f5e18a94fdfa7
Haritima Manchanda <haritima2308@gmail.com>
2025-06-22 10:23:05 -0700

    Getting started with Constructor Injection

--- learn-spring-framework/learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
+++ learn-spring-framework/learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DepInjectionLauncherApplication.java
@@ -9,25 +9,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 
-// Making use of Field injection - No setter or constructor. Dependency is injected using reflection.
+// Making use of Constructor injection.
 @Component
 class YourBusinessClass{
 	Dependency1 dependency1;
 	Dependency2 dependency2;
 	
-
 	@Autowired
-	public void setDependency1(Dependency1 dependency1) {
-		System.out.println("Setter Injection - setDependency1");
-		this.dependency1 = dependency1;
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+			super();
+			System.out.println("Constructor Injection - YourBusinessClass");
+			this.dependency1 = dependency1;
+			this.dependency2 = dependency2;
 	}
 
 
-	@Autowired
-	public void setDependency2(Dependency2 dependency2) {
-		System.out.println("Setter Injection - setDependency2");
-		this.dependency2 = dependency2;
-	}
+
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		System.out.println("Setter Injection - setDependency1");
+//		this.dependency1 = dependency1;
+//	}
+//
+//
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		System.out.println("Setter Injection - setDependency2");
+//		this.dependency2 = dependency2;
+//	}
 
 
 
